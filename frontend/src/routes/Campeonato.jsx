b0648c88928f434f57294b6a3c09a9c733c9a1db
matheus <matheusbispo0404@gmail.com>
2025-09-09 16:12:19 -0300

    Campeonato page done

--- frontend/src/routes/Campeonato.jsx
+++ frontend/src/routes/Campeonato.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import Tabela from '../assets/Tabela.png'
 
 const Campeonato = () => {
   return (
-    <div>Campeonato</div>
+    <main>
+      <h2>Campeonato Passa Bola</h2>
+      <div>
+        <aside>
+          <h3>Regras</h3>
+          <p>
+            Cada Equipe deve conter no mínimo 11 Jogadoras
+            Idade Máxima conforme a categoria.
+            Documentação Obrigatória para a inscrição.
+          </p>
+        </aside>
+        <aside>
+          <h2>Tabela do Campeonato</h2>
+          <img src={Tabela} alt='Tabela'></img>
+        </aside>
+      </div>
+      <div>
+        <article>
+          <h3>Inscreva-se</h3>
+          <input
+            type='text'
+            placeholder='Nome da Equipe'
+          />
+          <input
+            type='text'
+            placeholder='Nome do Técnico'
+          />
+          <input
+            type='text'
+            placeholder='Email'
+          />
+          <input
+            type='text'
+            placeholder='Telefone'
+          />
+          <button>Fazer Inscrição</button>
+        </article>
+        <div>
+          <h3>Informações do Campeonato</h3>
+          <label>Data: 15 de Julho de 2024</label>
+          <label>Estádio Municipal</label>
+          <label>16 Times</label>
+          <label>Eliminação Direta</label>
+          <label>Sub-15, Sub-17, Sub-20</label>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <main>
-        <div>
-            <h2>Elas em Campo</h2>
-            <p>Conectando meninas ao Futebol</p>
-        </div>
-        <article>
+    return (
+        <main>
             <div>
-                <h3>Peneiras</h3>
-                <p>Encontre peneiras próximas a você e se inscreva para participar das seleções</p>
-                <button/>
-            {/* Botão para clicar e ser direcionado para a Página que deseja */}
+                <h2>Elas em Campo</h2>
+                <p>Conectando meninas ao Futebol</p>
             </div>
-        </article>
-        <article>
-            <div>
-                <h3>Notícias</h3>
-                <p>Fique por dentro das novidades do Futebol Feminino e da Copa Passa Bola</p>
-                <button/>
-            {/* Botão para clicar e ser direcionado para a Página que deseja */}
-            </div>
-        </article>
-        <article>
-            <div>
-                <h3>Campeonato</h3>
-                <p>Acompanhe de perto os jogos do campeonato, e seja parte dessa História!</p>
-                <button/>
-            {/* Botão para clicar e ser direcionado para a Página que deseja */}
-            </div>
-        </article>
-    </main>
-  )
+            <article>
+                <div>
+                    <h3>Peneiras</h3>
+                    <p>Encontre peneiras próximas a você e se inscreva para participar das seleções</p>
+                    <button />
+                    {/* Botão para clicar e ser direcionado para a Página que deseja */}
+                </div>
+            </article>
+            <article>
+                <div>
+                    <h3>Notícias</h3>
+                    <p>Fique por dentro das novidades do Futebol Feminino e da Copa Passa Bola</p>
+                    <button />
+                    {/* Botão para clicar e ser direcionado para a Página que deseja */}
+                </div>
+            </article>
+            <article>
+                <div>
+                    <h3>Campeonato</h3>
+                    <p>Acompanhe de perto os jogos do campeonato, e seja parte dessa História!</p>
+                    <button />
+                    {/* Botão para clicar e ser direcionado para a Página que deseja */}
+                </div>
+            </article>
+        </main>
+    )
 }
 
 export default Home
