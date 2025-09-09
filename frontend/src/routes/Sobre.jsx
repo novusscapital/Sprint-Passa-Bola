4124193945f3d5727dbc1c83b45a5042387d177b
matheus <matheusbispo0404@gmail.com>
2025-09-09 16:12:45 -0300

    Sobre done, adding the Tabela image for Sobre

--- frontend/src/routes/Sobre.jsx
+++ frontend/src/routes/Sobre.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 
 const Sobre = () => {
-  return (
-    <main>
-        <h1>Sobre Nós</h1>
+    return (
+        <main>
+            <h2>Sobre Nós</h2>
+            <div>
+                <h2>Nossa Missão</h2>
+                <p>
+                    Elas em campo tem como missão democratizar o acesso ao futebol feminino no Brasil. Nosso objetivo é conectar meninas de todas as regiões as oportunidades no esporte, proporcionando uma plataforma segura e educativa que promove a inclusão e a visibilidade do futebol feminino.
+                </p>
+            </div>
 
-    </main>
-  )
+            <div>
+                <h2>Segurança</h2>
+                <p>
+                    Elas em Campo, a criação de contas é aberta até para crianças, com uso controlado e seguro. Não coletamos dados sensíveis nem compartilhamos informações pessoais. O ambiente é protegido, sem redes sociais públicas nem áreas de doação. Tudo é pensado para garantir privacidade, segurança e bem-estar.
+                </p>
+            </div>
+
+            <aside>
+                <h2>5+</h2>
+                <h3>Campeonatos</h3>
+
+                <h2>50+</h2>
+                <h3>Equipes</h3>
+
+                <h2>300+</h2>
+                <h3>Jogadoras</h3>
+            </aside>
+        </main>
+    )
 }
 
 export default Sobre
